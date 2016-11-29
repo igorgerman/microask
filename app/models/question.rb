53e8c757af4852f89813be5f01fd79577fe259ac
Raghu Betina <raghu@firstdraft.com>
2016-11-29 16:29:14 +0000

    Validate presence of questions.option_2

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,6 +5,8 @@ class Question < ApplicationRecord
 
   # Validations
 
+  validates :option_2, :presence => true
+
   validates :restaurant_id, :presence => true
 
   validates :text, :presence => true
